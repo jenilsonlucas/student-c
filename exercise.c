f9b5341b38e78370ee7d09b1eca5290065c48491
Jenilson Domingos Da Costa Lucas <jenilsonllucas@gmail.com>
2025-02-06 16:54:23 +0100

    serie de pi incompleto

--- exercise.c
+++ exercise.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846
+#define ERRO 1.0e-18
 
 
-float rounded(float number)
-{
-    int inteiro = (int)number;
-    float decimal = number - inteiro;
+double potencia(double base, int exp){
 
-    if(decimal >= 0.0 && decimal <= 0.24)
-        return inteiro + 0.25;
-    if(decimal <= 0.54)
-        return inteiro + 0.55;
-    if(decimal <= 0.74)
-        return inteiro + 0.75;
-    if(decimal <= 0.99)
-        return inteiro + 1.0;
+    double result = 1.0;
+    for(int i = 0; i < exp; i++){
+        result *= base;
+    }
+
+    return result;
+}
+double calculatorPi(){
+    double pi_aprox = 0.0;
+    double termo = 1.0/2.0;
+    int i = 1;
+
+
+    do{
+        termo *= ((2.0 * i - 1) / (2.0 * i)) * (1.0/(2.0*i + 1.0)); //* potencia(1.0/2.0, (2*i+1 ));
+        printf("valor do termo %lf\n", termo);
+        pi_aprox += termo;
+        i++;
+    }while(fabs(6.0 * pi_aprox - PI) <= ERRO);    
+
+    return pi_aprox * 6.0;
 }
 
 int main()
 {
-    float n = rounded(1.90);
+   printf("\nvalor de pi aproximado %lf\n", calculatorPi());
+   printf("valor de pi %lf\n", PI);
+   printf("valor de erro %lf\n", ERRO);
 
-    printf("the value rounded: %.2f\n", n);
+
     return 0;
 }
